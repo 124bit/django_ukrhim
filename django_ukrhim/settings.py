81b31586c58405ba105da7e0fb3f76ff4363cb9e
root <root@ip-172-31-19-251.us-west-2.compute.internal>
2013-07-23 08:12:59 +0000

    pdf gen see files, admintools normal static path

--- django_ukrhim/settings.py
+++ django_ukrhim/settings.py
@@ -329,7 +329,7 @@ ADMIN_TOOLS_MENU = 'modifier.custom_menu.CustomMenu'
 ADMIN_TOOLS_INDEX_DASHBOARD = 'modifier.custom_dashbord.CustomIndexDashboard'
 ADMIN_TOOLS_APP_INDEX_DASHBOARD = 'modifier.custom_dashbord.CustomAppIndexDashboard'
 
-ADMIN_TOOLS_THEMING_CSS = 'admintools/css/admintools_theming.css'
+ADMIN_TOOLS_THEMING_CSS = 'admin_tools_override/css/admintools_theming.css'
 #A good start is to copy the admin_tools/media/admin_tools/css/theming.css to your custom file and to modify it to suits your needs.
 
 
--- pdf_gen/models.py
+++ pdf_gen/models.py
@@ -15,7 +15,7 @@ from eav.fields import EavSlugField
 from django.contrib.admin.models import LogEntry, CHANGE
 import platform
 class Price(models.Model):
-    prices_path=path.join(settings.PROJECT_PATH,settings.MEDIA_ROOT,'files','generated_prices')
+    prices_path=path.join('django_ukrhim/media/files','generated_prices')
 
     name=EavSlugField(_("Document name"), max_length=50, help_text=_("Use this name like '{% load price_file %} {% price_file site='auto' lang='auto' %}'." ),
                    unique=True)
@@ -80,7 +80,7 @@ class PriceTemplate(models.Model):
         res_path=path.join(folder, self.get_price_name())
         context=self.get_template_context()
         if platform.system() == 'Linux':
-            template=settings.PROJECT_PATH+self.template_file.url
+            template='django_ukrhim/'+self.template_file.url
             renderer = Renderer(template, context, res_path,  overwriteExisting=True)
         else:
             template=path.join(settings.PROJECT_PATH,self.template_file.url.replace('/','\\')[1:])
